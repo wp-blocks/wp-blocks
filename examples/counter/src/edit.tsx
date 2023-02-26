84395bf66757ba4cb48e4cdb76ffd2eb92934237
John Hooks <bitmachina@outlook.com>
2023-02-25 16:43:18 -0800

    feat: initial enqueue React example

---
+++ examples/counter/src/edit.tsx
@@ -0,0 +1,33 @@
+/**
+ * React hook that is used to mark the block wrapper element.
+ * It provides all the necessary props like the class name.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
+ */
+import { useBlockProps } from '@wordpress/block-editor';
+import type { WPElement } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * Those files can contain any CSS code that gets applied to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import './editor.scss';
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
+ *
+ * @return Element to render.
+ */
+export default function Edit(): WPElement {
+	return (
+		<p { ...useBlockProps() }>
+			{ __( 'Counter – hello from the editor!', 'counter' ) }
+		</p>
+	);
+}
