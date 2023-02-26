e62632564642982791fe23758a5f24ee2215456a
John Hooks <bitmachina@outlook.com>
2023-02-26 10:28:48 -0800

    ci: add GitHub workflow (#15)
    * ci: add basic github workflow
    
    * chore: remove create-block package
    
    * chore: fix formatting

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,5 +1,9 @@
 module.exports = {
 	'**/*.(ts|tsx)': () => 'pnpm check',
+	'**/*.(json|yml|yaml)': ( filenames ) => {
+		const files = filenames.join( ' ' );
+		return [ `prettier --write ${ files }` ];
+	},
 	'**/*.(js|jsx|cjs|mjs|ts|tsx)': ( filenames ) => {
 		const files = filenames.join( ' ' );
 		return [ `eslint --ext .js,.jsx,.cjs,.mjs,.ts,.tsx --fix ${ files }` ];
