9aefdce6437b901e3e1caa0b5866457251de3a6a
John Hooks <bitmachina@outlook.com>
2023-03-18 07:45:03 -0700

    fix: remove yarn from scripts

--- scripts/build-pkg.js
+++ scripts/build-pkg.js
@@ -3,13 +3,13 @@
 import { exec } from './utils/exec.js';
 
 if ( ! process.env.CI ) {
-	await exec( 'npm', [ 'run', 'clean:all' ] );
+	await exec( 'pnpm', [ 'run', 'clean:all' ] );
 }
 
 try {
-	await exec( 'npm', [ 'run', 'build:compile' ] );
-	await exec( 'npm', [ 'run', 'build:esm' ] );
-	await exec( 'npm', [ 'run', 'build:types' ] );
+	await exec( 'pnpm', [ 'run', 'build:compile' ] );
+	await exec( 'pnpm', [ 'run', 'build:esm' ] );
+	await exec( 'pnpm', [ 'run', 'build:types' ] );
 } catch ( code ) {
 	process.exit( code );
 }
